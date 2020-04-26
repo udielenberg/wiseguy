a061f83bb7ab9ffe818b68cf32d65ab9c41a7016
udielenberg <udielenberg@gmail.com>
2020-04-26 16:04:56 +0300

    add setNote  functionality

--- packages/web/src/components/AddNote/AddNote.tsx
+++ packages/web/src/components/AddNote/AddNote.tsx
@@ -1,11 +1,29 @@
-import React from "react";
-import { Input } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Input, InputOnChangeData } from "semantic-ui-react";
 import styled from "styled-components";
 
 export const AddNote = () => {
+  const [note, setNote] = useState<string>();
+
+  const handleChange = (_: any, data: InputOnChangeData) => {
+    setNote(data.value);
+  };
+  const handleEnter = (event: any) => {
+    const value = note && note.trim();
+    if (event.key === "Enter" && value) {
+      setNote("");
+    }
+  };
   return (
     <Wrapper>
-      <Input size="huge" icon="add" placeholder="Add note..." />
+      <Input
+        size="huge"
+        icon="add"
+        value={note}
+        placeholder="Add note..."
+        onChange={handleChange}
+        onKeyDown={handleEnter}
+      />
     </Wrapper>
   );
 };
