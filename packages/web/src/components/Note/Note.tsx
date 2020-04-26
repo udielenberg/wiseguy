81187813efe42cb598356852d278f16c5d98e661
udielenberg <udielenberg@gmail.com>
2020-04-26 20:55:19 +0300

    export Note component

---
+++ packages/web/src/components/Note/Note.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Note as TNote } from "dummydata/notes";
+import { Table as SemanticUITable } from "semantic-ui-react";
+import styled from "styled-components";
+import { Label } from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
+
+interface Header {
+  type: string;
+  header: string;
+}
+interface Props {
+  note: TNote;
+  headers: Header[];
+  remove(id: string): void;
+  open(id: string): void;
+}
+const { Header, Cell, Row } = SemanticUITable;
+
+export const Note = ({ note, remove, headers, open }: Props) => {
+  return (
+    <Row>
+      {headers.map(({ type }) => {
+        if (type === "lastVisit" || type === "date") {
+          return <DateCell>{note[type].toDateString()}</DateCell>;
+        } else if (type === "search") {
+          return <Cell>{note[type]}</Cell>;
+        } else if (type === "tags") {
+          return (
+            <Cell>
+              {note[type].map((tag) => (
+                <Label key={tag}>{tag}</Label>
+              ))}
+            </Cell>
+          );
+        } else if (type === "open") {
+          return (
+            <Cell>
+              <Button
+                primary
+                onClick={() => {
+                  open(note.id);
+                }}
+              >
+                Open
+              </Button>
+            </Cell>
+          );
+        } else {
+          return (
+            <Cell>
+              <Button
+                negative
+                onClick={() => {
+                  remove(note.id);
+                }}
+              >
+                Remove
+              </Button>
+            </Cell>
+          );
+        }
+      })}
+    </Row>
+  );
+};
+
+const DateCell = styled(Cell)`
+  font-size: 14px;
+  font-weight: 100;
+`;
--- packages/web/src/components/NotesTable/NotesTable.tsx
+++ packages/web/src/components/NotesTable/NotesTable.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Table as SemanticUITable } from "semantic-ui-react";
-import { notes, Note } from "dummydata/notes";
 import capitalize from "lodash/capitalize";
-import { Label } from "semantic-ui-react";
-import { Button } from "semantic-ui-react";
 import styled from "styled-components";
-
-const { Header, HeaderCell, Row, Cell, Body } = SemanticUITable;
+import { Note as TNote } from "dummydata/notes";
+import { Note } from "components/Note";
+const { Header, HeaderCell, Row, Body } = SemanticUITable;
 
 interface Header {
   type: string;
@@ -16,72 +14,18 @@ const headers: Header[] = [
   { type: "search", header: "search" },
   { type: "tags", header: "tags" },
   { type: "lastVisit", header: "last time visited" },
-  { type: "fetchMore", header: "fetch more" },
+  { type: "open", header: "fetch more" },
   { type: "remove", header: "remove" },
   { type: "date", header: "created  " },
 ];
 
-const createDate = (date: Date) => <DateCell>{date.toDateString()}</DateCell>;
+interface Props {
+  notes: TNote[];
+  remove(id: string): void;
+  open(id: string): void;
+}
 
-const createSearch = (search: string) => <Cell>{search}</Cell>;
-
-const createTags = (tags: string[]) => {
-  return (
-    <Cell>
-      {tags.map((tag) => (
-        <Label key={tag} tag>
-          {tag}
-        </Label>
-      ))}
-    </Cell>
-  );
-};
-
-const fetchMore = () => {
-  return (
-    <Cell>
-      <Button primary>Fetch more</Button>
-    </Cell>
-  );
-};
-const remove = () => {
-  return (
-    <Cell>
-      <Button negative>Remove</Button>
-    </Cell>
-  );
-};
-
-const createCell = (headerType: string, note: Note) => {
-  switch (headerType) {
-    case "lastVisit":
-    case "date":
-      return createDate(note[headerType]);
-    case "search":
-      return createSearch(note[headerType]);
-    case "tags":
-      return createTags(note[headerType]);
-    case "fetchMore":
-      return fetchMore();
-    case "remove":
-      return remove();
-    default:
-      throw Error("something when wrong");
-  }
-};
-
-const createNote = (note: any, headers: Header[]) => {
-  const { id, ...noteRest } = note;
-  return (
-    <Row key={id}>
-      {headers.map(({ type }) => {
-        return createCell(type, noteRest);
-      })}
-    </Row>
-  );
-};
-
-export const NotesTable = () => {
+export const NotesTable = ({ notes, remove, open }: Props) => {
   return (
     <Table celled size="large" sortable={true}>
       <Header>
@@ -94,15 +38,15 @@ export const NotesTable = () => {
         </Row>
       </Header>
 
-      <Body>{notes.map((note) => createNote(note, headers))}</Body>
+      <Body>
+        {notes &&
+          notes.map((note) => (
+            <Note key={note.id} {...{ note, remove, open, headers }} />
+          ))}
+      </Body>
     </Table>
   );
 };
-
-const DateCell = styled(Cell)`
-  font-size: 14px;
-  font-weight: 100;
-`;
 
 const Table = styled(SemanticUITable)`
   margin: auto !important;
