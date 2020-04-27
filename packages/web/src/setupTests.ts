1a0320b10811572167c98ee5f3f90e830535ff45
udielenberg <udielenberg@gmail.com>
2020-04-27 15:23:49 +0300

    add setupTest file

--- packages/web/src/setupTests.ts
+++ packages/web/src/setupTests.ts
@@ -1,8 +1,8 @@
 import React from "react";
 import { render, cleanup, act, fireEvent } from '@testing-library/react'
+
 const globalAny: any = global;
 
-globalAny.React = React;
 globalAny.render = render;
 globalAny.cleanup = cleanup;
 globalAny.fireEvent = fireEvent;
