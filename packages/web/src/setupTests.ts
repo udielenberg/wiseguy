41eaa6744cd9f4810b9e7a63b03404e72ef2f074
udielenberg <udielenberg@gmail.com>
2020-04-27 15:11:14 +0300

    add setupTests file

--- packages/web/src/setupTests.ts
+++ packages/web/src/setupTests.ts
@@ -1,5 +1,9 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import React from "react";
+import { render, cleanup, act, fireEvent } from '@testing-library/react'
+const globalAny: any = global;
+
+globalAny.React = React;
+globalAny.render = render;
+globalAny.cleanup = cleanup;
+globalAny.fireEvent = fireEvent;
+globalAny.act = act;
