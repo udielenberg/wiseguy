b258a58bcbcb83d4c9df30ffa335bb19be8c55d8
udielenberg <udielenberg@gmail.com>
2020-05-03 23:26:47 +0300

    update models

--- packages/web/src/models/Note.ts
+++ packages/web/src/models/Note.ts
@@ -1,16 +1,31 @@
 import faker from 'faker';
 
+export type ResourceState = "fresh" | "undecided" | "approved" | "rejected"
+export interface Resource {
+    id: string;
+    state: ResourceState;
+    tags: string[];
+    rating: number;
+    link: string;
+    description: string;
+    createdAt: Date;
+    updatedAt?: Date;
+    images?: string[];
+    writtenBy: string;
+    readingTime: number;
+}
 export interface Note {
     id: string;
     created: Date;
     search: string;
     tags: Array<string>;
     lastVisit: Date;
-    watched: boolean
-    resources: any[], //RETHINK THIS
-    approved: any[], //RETHINK THIS
-    rejected: any[], //RETHINK THIS
+    watched: boolean;
+    resources: Resource[];
+
 }
+
+export const resourceStates: ResourceState[] = ["fresh", "undecided", "approved", "rejected"];
 
 export const baseNote: Note = {
     id: faker.random.uuid(),
@@ -19,7 +34,18 @@ export const baseNote: Note = {
     tags: [],
     lastVisit: new Date(),
     watched: false,
-    resources: [], //RETHINK THIS
-    approved: [], //RETHINK THIS
-    rejected: [], //RETHINK THIS
+    resources: [],
+}
+
+export const baseResource: Resource = {
+    id: faker.random.uuid(),
+    state: "fresh",
+    tags: [],
+    link: '',
+    rating: 0,
+    description: '',
+    images: [],
+    createdAt: new Date(),
+    readingTime: 0,
+    writtenBy: ''
 }
