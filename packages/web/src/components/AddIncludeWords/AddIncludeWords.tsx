28493b0df2c8fe5de5df79d7cfbd9800790a35e9
udielenberg <udielenberg@gmail.com>
2020-05-04 15:22:05 +0300

    add component

---
+++ packages/web/src/components/AddIncludeWords/AddIncludeWords.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import CreatableSelect from "react-select/creatable";
+
+const colourOptions: any = [];
+
+interface Props {
+  add(words: { label: string; value: string }[]): void;
+}
+
+export const AddIncludeWords = (props: Props) => {
+  const { add } = props;
+  const [words, setWords] = useState<any>([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (options: any) => {
+    setWords(options);
+  };
+
+  const handleInputChange = (input: any) => {
+    setInputValue(input);
+  };
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLElement>) => {
+    if (event.key === "Enter" || event.key === "Tab") {
+      const newWord = { value: inputValue, label: inputValue };
+      const updatedWords = [...words, newWord];
+      setWords(updatedWords);
+      setInputValue("");
+      add(updatedWords.filter(({ label }) => label));
+    }
+  };
+  return (
+    <Wrapper>
+      <CreatableSelect
+        isMulti
+        onChange={handleChange}
+        onInputChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+        options={colourOptions}
+        value={words}
+        inputValue={inputValue}
+        menuIsOpen={false}
+        isClearable
+        components={{ DropdownIndicator: null }}
+      />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  margin: 20px;
+  padding: 10px;
+  text-align: center;
+`;
---
+++ packages/web/src/components/AddIncludeWords/index.ts
@@ -0,0 +1,1 @@
+export { AddIncludeWords } from './AddIncludeWords'
