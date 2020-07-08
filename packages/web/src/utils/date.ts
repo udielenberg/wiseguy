3feb2052f420ff84e0d571cbbb32b8cb4331f46e
udielenberg <udielenberg@gmail.com>
2020-07-08 11:08:05 +0300

    add error handling to date

--- packages/web/src/utils/date.ts
+++ packages/web/src/utils/date.ts
@@ -2,4 +2,11 @@ import { format } from "date-fns";
 
 const MM_DD_YYYY_FORMAT = 'MM/dd/yyyy';
 
-export const formattedDate = (date: Date) => format(date, MM_DD_YYYY_FORMAT)
+export const formattedDate = (date: Date) => {
+    try {
+        return format(date, MM_DD_YYYY_FORMAT)
+    } catch (err) {
+        console.error(`invalid date object:', ${date}`);
+        return 'invalid date'
+    }
+}
